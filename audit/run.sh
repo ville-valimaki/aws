ab36860c26a219afce0dc04a4c3cb621788ffa1f
Ville Välimäki <vjvalima@gmail.com>
2023-03-02 20:33:55 +0200

    improvement: default override values

--- audit/run.sh
+++ audit/run.sh
@@ -3,14 +3,14 @@
 source ./scripts/unused-ebs-volumes.sh
 source ./scripts/unattached-network-interfaces.sh
 
-# echo Enter region for audit:
-# read REGION
-# echo Enter profile for audit:
-# read PROFILE
+echo Enter region for audit:
+read REGION
+echo Enter profile for audit:
+read PROFILE
 
 # for quick default values
-REGION=eu-north-1
-PROFILE=vjvalima
+# REGION=eu-north-1
+# PROFILE=vjvalima
 
 echo Running audit with profile $PROFILE in region $REGION
 
